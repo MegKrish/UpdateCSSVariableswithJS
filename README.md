7d5301b1aa5c433fa875a6bb38f48a75b0fdf98a
MegKrish <44385720+MegKrish@users.noreply.github.com>
2019-05-26 15:15:12 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# UpdateCSSVariableswithJS
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,23 @@
+// import "./styles.css";
+
+// document.getElementById("app").innerHTML = `
+// <h1>Hello Vanilla!</h1>
+// <div>
+//   We use Parcel to bundle this sandbox, you can find more info about Parcel
+//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+// </div>
+// `;
+
+const inputs = document.querySelectorAll(".controls input");
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || "";
+
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
